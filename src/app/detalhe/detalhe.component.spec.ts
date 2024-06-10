a3ca54b7bdd7425b962ec9cf0127f5fa22c839b2
Charleston J. Campos <charlinhocjc@gmail.com>
2024-06-10 10:26:43 -0300

    Vídeo 2.1 - Criação dos testes para a tela de Detalhes

--- src/app/detalhe/detalhe.component.spec.ts
+++ src/app/detalhe/detalhe.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetalheComponent } from './detalhe.component';
+import { SharedModule } from '@shared/shared.module';
+import { DepoimentosModule } from '../home/depoimentos/depoimentos.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DetalheComponent', () => {
   let component: DetalheComponent;
@@ -8,7 +12,17 @@ describe('DetalheComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DetalheComponent]
+      declarations: [
+        DetalheComponent
+      ],
+      imports: [
+        SharedModule,
+        DepoimentosModule,
+        HttpClientTestingModule
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     });
     fixture = TestBed.createComponent(DetalheComponent);
     component = fixture.componentInstance;
